447ce7497b49b366659741f652510227789f89e1
chinaysun <ChinaYuSun@suns-macbook-air.local>
2017-08-09 12:29:28 +1000

    Initial Commit

---
+++ InstantChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstantChat
+//
+//  Created by SUN YU on 9/8/17.
+//  Copyright © 2017 SUN YU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
