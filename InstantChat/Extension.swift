e5a6903fee84545397674aeb5aadd3066b5f969c
YU SUN <chinaysun@gmail.com>
2017-08-12 11:47:32 +1000

    Image Download & Caching

---
+++ InstantChat/Extension.swift
@@ -0,0 +1,60 @@
+//
+//  Extension.swift
+//  InstantChat
+//
+//  Created by SUN YU on 11/8/17.
+//  Copyright © 2017 SUN YU. All rights reserved.
+//
+
+import UIKit
+
+let imageCache = NSCache<NSString, AnyObject>()
+
+extension UIImageView
+{
+    
+    func loadImageUsingCachewithUrlString(urlString:String)
+    {
+        self.image = nil
+        
+        //check cache for image first
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) as? UIImage
+        {
+            self.image = cachedImage
+            return
+        }
+        
+        
+        //otherwise download the image
+        let url = URL(string: urlString)
+        
+        URLSession.shared.dataTask(with: url!, completionHandler: {
+            
+            (data,response,error)
+            
+            in
+            
+            if error != nil
+            {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.main.async(execute: {
+                
+                if let downloadedImage = UIImage(data: data!)
+                {
+                    imageCache.setObject(downloadedImage, forKey: urlString as NSString)
+                }
+                
+                self.image = UIImage(data: data!)
+            
+            
+            })
+            
+            
+            
+        }).resume()
+    }
+    
+}
--- InstantChat/NewMessageController.swift
+++ InstantChat/NewMessageController.swift
@@ -63,14 +63,25 @@ class NewMessageController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserCell
         
         let user = users[indexPath.row]
         cell.textLabel?.text = user.name
         cell.detailTextLabel?.text = user.email
         
+        
+        if let profileImageUrl = user.profileImageUrl
+        {
+            cell.profileImageView.loadImageUsingCachewithUrlString(urlString: profileImageUrl)
+            
+        }
+        
         return cell
         
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
     }
 
 }
@@ -79,8 +90,39 @@ class NewMessageController: UITableViewController {
 
 class UserCell:UITableViewCell
 {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 64, y: (textLabel?.frame.origin.y)! - 2, width: (textLabel?.frame.width)!, height: (textLabel?.frame.height)!)
+        
+        detailTextLabel?.frame = CGRect(x: 64, y: (detailTextLabel?.frame.origin.y)! + 2, width: (detailTextLabel?.frame.width)!, height: (detailTextLabel?.frame.height)!)
+    }
+    
+    let profileImageView:UIImageView = {
+       
+        let imageView = UIImageView()
+        imageView.image = UIImage(named:"logo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 24
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+        
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: UITableViewCellStyle.subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        
+        //need x,y,width,height anchors
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
